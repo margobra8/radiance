12ff6f5851027737d8c174fcce8d61def51139e8
Marcos Gómez <margobra8@gmail.com>
2017-01-26 01:19:46 +0100

    fixed polls regex and added preliminary views

--- rad/urls.py
+++ rad/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r"polls", include("radiance.url"))
+    url(r"^polls/", include("radiance.urls"))
 ]
--- radiance/views.py
+++ radiance/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("testing!")
+
+
+def detail(request, question_id):
+    return HttpResponse("this is the detail vew of the question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("this are the results of the question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("vote on question %s" % question_id)
